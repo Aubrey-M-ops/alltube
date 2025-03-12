ad82f2734bd820fd9850aa896a2f1ac736aa0e28
ZiWei Rao <r3033595562@gmail.com>
2025-03-11 20:27:37 -0400

    Feat: Create the video list on Home page

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,9 +1,91 @@
 import React from "react";
 import "./index.scss";
 
+import HomeVideoCard from "@components/HomeVideoCard";
+
+const video_data = [
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  },
+  {
+    video_screenshot_url: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    avatar_url: "https://api.dicebear.com/7.x/miniavs/svg?seed=8",
+    video_title: "Video Title",
+    video_description: "Here is the video description"
+  }
+];
+
 const HomePage = () => {
   return (
     <div className="home-page">
+      {
+        video_data.map((item, index) => (
+          <HomeVideoCard video_screenshot_url={item.video_screenshot_url} avatar_url={item.avatar_url} video_title={item.video_title} video_description={item.video_description} />
+        ))
+      }
     </div>
   );
 };
