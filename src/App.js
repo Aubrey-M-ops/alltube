452027ba485342e04348bd02282c1db52d08a609
mohan <mohanli202409@gmail.com>
2025-03-26 20:49:22 -0400

    feat: playlist complete

--- src/App.js
+++ src/App.js
@@ -12,13 +12,42 @@ import VideoPage from "pages/videoPage";
 import TopBar from "@components/TopBar";
 import SideBar from "@components/SideBar";
 import { Layout } from "antd";
-import { useState } from "react";
 
 import "./App.css";
 import { video_data } from "data/VideoData";
 import { playlist_data } from "data/PlayListData";
 
 const { Sider, Content } = Layout;
+
+function AppLayout() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search); 
+  const video_id = searchParams.get("video_id"); 
+  const videoItem = video_data.find(
+    (video) => video.video_id === Number(video_id)
+  );
+  const isPlayerPage = location.pathname.startsWith("/video");
+console.log(videoItem,video_id, 123123213)
+  return (
+    <div className="App">
+      <TopBar />
+      <Layout>
+        {!isPlayerPage && (
+          <Sider>
+            <SideBar />
+          </Sider>
+        )}
+        <Content className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/playlist" element={<Playlist />} />
+            <Route path="/video" element={<VideoPage videoItem={videoItem} />} />
+          </Routes>
+        </Content>
+      </Layout>
+    </div>
+  );
+}
 
 function App() {
   return (
